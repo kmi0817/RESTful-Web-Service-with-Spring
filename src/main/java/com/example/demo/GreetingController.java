9e85b9922e4e3050329fdd7253b3797230a87ee0
kmi0817 <kmi0817@naver.com>
2022-03-11 23:48:28 +0900

    chore: :pencil2: fix the typo in importing java util
    In line 3, [import java.until.concurrent.atomic.AtomicLong; ], there was a typo "until". I couldn't run the application with an error "Failed to execute goal org.apache.maven.plugins:maven-compiler-plugin:3.8.1:compile (default-compile) on project demo: Compilation failure: Compilation failure:" because of this typo.  After changing"until" to "util", this application runs well.

--- src/main/java/com/example/demo/GreetingController.java
+++ src/main/java/com/example/demo/GreetingController.java
@@ -1,6 +1,6 @@
 package com.example.demo;
 
-import java.until.concurrent.atomic.AtomicLong;
+import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
